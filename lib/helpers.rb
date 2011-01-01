1c81f18252bbc8d584792024e12ba0e946b82779
Jay Adkisson <jjmadkisson@gmail.com>
2010-12-31 21:46:02 -0800

    rm debugging code

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,17 +1,8 @@
 module Helpers
   def colorize(body)
-puts '========== DEBUG body ========='
-puts body
-puts '==========end debug body ======'
     doc = Hpricot(body)
     doc.search('pre').each do |pre|
       code = pre.children[0].innerHTML
-
-puts "code"
-puts code
-puts '======== DEBUG pre ==========='
-puts pre.to_html
-puts '===== end DEBUG pre =========='
 
       style, code = grab_style(code)
 
@@ -31,12 +22,7 @@ puts '===== end DEBUG pre =========='
 
     lines = code.split "\n"
 
-p :lines_pre => lines
-
     lines.shift =~ /@@\s*(\w+)/
-
-p :lines => lines,
-  :match => $1
 
     [:"#{$1}", lines.join("\n")]
   end
